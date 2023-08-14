1f199a72f4c86c425046bede4017b25c0e13742f
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-08-14 23:37:51 +0530

    typecasting

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+
+import java.util.Scanner;
+
 public class Main {
 
-public static void main (String[] args){
-  System.out.println("Hello World");
-}
+   
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    // System.out.println("hello aditya");
+    // int rollno = input.nextInt();
+    // System.out.println("your rollno is " + rollno);
+    // int a = 234_000_000;
+    // System.out.println(a);
+
+    // String name = input.next();
+    // System.out.println(name); 
+    
+    // float marks = input.nextFloat();
+    // System.out.println(marks);
+
+    
+
+  }
 }
---
+++ typecasting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class typecasting {
+  public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+// float num = input.nextFloat();
+// int num = input.nextInt();
+// System.out.println(num);
+
+//type casting
+int num = (int)(64.5f);
+System.out.println(num);
+
+  }
+}
